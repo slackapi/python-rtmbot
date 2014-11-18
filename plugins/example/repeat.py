29fbb30c75a5175edfc802abbc39928f005d3e5e
Ryan Huber <rhuber@gmail.com>
2014-11-17 17:30:38 -0800

    remove prints on example

--- plugins/example/repeat.py
+++ plugins/example/repeat.py
@@ -3,7 +3,6 @@ crontable = []
 outputs = []
 
 def process_message(data):
-    print data
     if data['channel'].startswith("D"):
         outputs.append([data['channel'], "from repeat1: " + data['text']])
 
--- plugins/example/repeat2.py
+++ plugins/example/repeat2.py
@@ -3,7 +3,6 @@ crontable = []
 outputs = []
 
 def process_message(data):
-    print data
     if data['channel'].startswith("D"):
         outputs.append([data['channel'], "from repeat2: " + data['text']])
 
