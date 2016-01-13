f94a73b7905e087d1305aa09b743e0b05631553a
Randall Barnhart <randall.barnhart@robotsandpencils.com>
2016-01-13 11:58:07 -0700

    Removed original fork documentation not relevant to BeepBoop.

--- doc/example-plugins/repeat.py
+++
@@ -1,8 +0,0 @@
-import time
-crontable = []
-outputs = []
-
-def process_message(data):
-    if data['channel'].startswith("D"):
-        outputs.append([data['channel'], "from repeat1 \"{}\" in channel {}".format(data['text'], data['channel']) ])
-
