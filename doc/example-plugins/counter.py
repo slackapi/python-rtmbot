b03752144c85a7dcb2fbda633f3a932cd9fbb1ad
Tiphaine Phe-Neau <tiph.pheneau@gmail.com>
2016-05-18 11:06:19 +0200

    Fix AttributeError in counter.py example plugin

--- doc/example-plugins/counter.py
+++ doc/example-plugins/counter.py
@@ -7,4 +7,4 @@ crontable.append([5, "say_time"])
 
 def say_time():
     # NOTE: you must add a real channel ID for this to work
-    outputs.append(["D12345678", time.time()])
+    outputs.append(["D12345678", str(time.time())])
