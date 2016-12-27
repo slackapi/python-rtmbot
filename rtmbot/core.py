b96ae946c9628c7c95b2198c1750ca7b1a451d43
Geoff Donaldson <gdonalds@lab126.com>
2016-12-27 03:29:45 -0600

    Fixing job_output bug.

--- rtmbot/core.py
+++ rtmbot/core.py
@@ -239,11 +239,9 @@ class Plugin(object):
 
                 # job attempted execution so reset the timer and log output
                 job.lastrun = time.time()
-                if job_output is not None:
-                    print("I'm not NONE!!!")
-                    print(job_output)
-                    for out in job_output:
-                        self.outputs.append(out)
+                # if job_output is not None:
+                #     for out in job_output:
+                #         self.outputs.append(out)
 
     def do_output(self):
         output = []
