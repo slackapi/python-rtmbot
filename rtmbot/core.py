ad1586b3265bb4a66190edd1a8aefa4338fc6e47
Andrew Erickson <aerickson@gmail.com>
2017-05-10 22:11:53 -0700

    fix another linter error

--- rtmbot/core.py
+++ rtmbot/core.py
@@ -125,7 +125,8 @@ class RtmBot(object):
                         result = json.loads(self.slack_client.api_call('im.open', user=destination))
                     except ValueError:
                         self._dbg("Parse error on im.open call results!")
-                    channel = self.slack_client.server.channels.find(result.get(u'channel', {}).get(u'id', None))
+                    channel = self.slack_client.server.channels.find(
+                        result.get(u'channel', {}).get(u'id', None))
                 elif destination.startswith('G'):
                     result = self.slack_client.api_call('groups.open', channel=destination)
                     channel = self.slack_client.server.channels.find(destination)
