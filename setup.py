d43f6f419f2789d8e87757c932ae0374bc05b330
roach <urbandev@gmail.com>
2018-02-05 14:59:50 -0800

    Updated Version and contact email

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='rtmbot',
-    version='0.4.0',
+    version='0.4.1',
     description='A Slack bot written in python that connects via the RTM API.',
     author='Ryan Huber and Jeff Ammons',
-    author_email='jeff@jeffammons.net',
+    author_email='developers@slack.com',
     url='https://github.com/slackhq/python-rtmbot',
     packages=find_packages(),
     entry_points={'console_scripts': ['rtmbot=rtmbot.bin.run_rtmbot:main']},
